84e2a2d64efbcf4ec1cd20d9a2394b8522fc2793
Pavel Krayzel <pavel.krayzel@made.com>
2019-02-20 22:16:31 +0100

    Merging script run for bolia

--- utils/merge_json.py
+++ utils/merge_json.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 COMPETITORS_PRODUCT_PER_FILE = [
     "bolia",
@@ -19,13 +20,32 @@ def main():
 
         for subdir, dirs, files in os.walk(start_dir):
 
-            for dir in dirs:
+            for directory in dirs:
+                category_directory = f"{start_dir}/{directory}/"
 
-                for subdir, dirs, files in os.walk(start_dir + "/" + dir):
+                for subdir, dirs, files in os.walk(category_directory):
 
-                    print(f"{dir} - number of files: {len(files)}")
-                    # for filename in files:
-                    #     print(filename)
+                    print(f"{category_directory} - number of files: {len(files)}")
+
+                    products_for_category = []
+
+                    for filename in files:
+                        full_path = f"{category_directory}{filename}"
+
+                        # read each product file
+                        with open(full_path) as f:
+                            product = json.load(f)
+                            products_for_category.append(product)
+
+                    output_filename = f"{start_dir}/{competitor}_{directory}.json"
+                    # write list of products for category to output file
+                    with open(output_filename, 'w') as output_file:
+                        json.dump(products_for_category, output_file, indent=4)
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
